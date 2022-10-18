cc4870af20d4777e79677c668e2118dbfb59bc5b
patrick-atienza25 <patienza25@gmail.com>
2022-10-18 09:17:09 -0700

    finished exercise 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -35,6 +35,7 @@ namespace OperatorExercise
             if (a == 17 && b == 4);
             Console.WriteLine({a} / {b} is {quotient} remainder {remainder});
 
+            var radius = double.Parse();
 
            public static double AreaOfCircle(oduble radius)
             {
