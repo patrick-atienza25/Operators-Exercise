036b84b98e8ef64bbf32b3361ecbac873ccc5fdd
patrick-atienza25 <patienza25@gmail.com>
2022-10-18 09:02:11 -0700

    finished exercise 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            //addition
+            int sum = a + b;
+            Console.WriteLine(sum);
+
+            //subtraction
+            int sub = a - b;
+            Console.WriteLine(sub);
+
+            //multiplication
+            int mult = a * b;
+            Console.WriteLine(mult);
+
+            //division
+            int div = a / b;
+            Console.WriteLine(div);
+
+            //modulus
+            int mod = a % b;
+            Console.WriteLine(mod);
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4);
+            Console.WriteLine({a} / {b} is {quotient} remainder {remainder});
+
+
+           public static double AreaOfCircle(oduble radius)
+            {
+                var area = Math.PI * Math.Pow(2, radius);
+
+                return area;
+            }
         }
     }
 }
